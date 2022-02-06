e9e6ab7c0a38b54ea933c58aa7bd424601e51c56
nyakiochristine <christine.mwangi@student.moringaschool.com>
2022-02-06 23:14:07 +0300

    feat:button-added the button feature

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ function myButton()
     var correctAnswer4=document.firstChild.number4.value;
     var correctAnswer5=document.firstChild.number5.value;
     var result=document.getElementById("text")
-    if (correctAnswer1="Array") {score+=20}
+    if (correctAnswer1=="Array") {score+=20};
+    if (correctAnswer2=="Script") {score+=20};
+    if (correctAnswer3=="const") {score+=20};
+    if (correctAnswer4=="Correct") {score+=20};
+    if (correctAnswer==30){score+=10};
+    firstChild.style.display="none";
+    result.textContent=score;
+    document.write(score);
 }
