5518e7ceb0ff89dcdcba9e1fa633ddd9a0ddf33e
nyakiochristine <christine.mwangi@student.moringaschool.com>
2022-02-07 00:14:07 +0300

    patch:fixed errors in the file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function myButton() 
+function myButton() {
+
+} 
 {
     var score=0
     var correctAnswer1=document.firstChild.number1.value;
@@ -6,13 +8,18 @@ function myButton()
     var correctAnswer3=document.firstChild.number3.value;
     var correctAnswer4=document.firstChild.number4.value;
     var correctAnswer5=document.firstChild.number5.value;
+    var test=getElementById("test")
     var result=document.getElementById("text")
     if (correctAnswer1=="Array") {score+=20};
     if (correctAnswer2=="Script") {score+=20};
     if (correctAnswer3=="const") {score+=20};
     if (correctAnswer4=="Correct") {score+=20};
-    if (correctAnswer==30){score+=10};
-    firstChild.style.display="none";
-    result.textContent=score;
-    document.write(score);
+    test.style.display="none";
+    
+    if (score>=4) {
+    result.textContent= "Your score is 80%. Congratulations";
+    } else{
+
+    }
+
 }
