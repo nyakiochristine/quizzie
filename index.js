053c54fbcf3f939c35caf6f91b12c8f86bcb0330
nyakiochristine <christine.mwangi@student.moringaschool.com>
2022-02-06 20:51:33 +0300

    feat-checkbox-added the checkboxes to the questions

---
+++ index.js
@@ -0,0 +1,11 @@
+function myButton() 
+{
+    var score=0
+    var correctAnswer1=document.firstChild.number1.value;
+    var correctAnswer2=document.firstChild.number2.value;
+    var correctAnswer3=document.firstChild.number3.value;
+    var correctAnswer4=document.firstChild.number4.value;
+    var correctAnswer5=document.firstChild.number5.value;
+    var result=document.getElementById("text")
+    if (correctAnswer1="Array") {score+=20}
+}
