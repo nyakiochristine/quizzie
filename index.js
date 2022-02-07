164f6cf241243a5b5255937e06b57433cb707a39
nyakiochristine <christine.mwangi@student.moringaschool.com>
2022-02-07 07:57:01 +0300

    bug-fixed errors in the code

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 function myButton() {
-    var Correct=0;
-    document.write(score);
-     var currentAnswer1=document.Quick.number1.value;
-    var currentAnswer2=document.Quick.number2.value;
-    var currentAnswer3=document.Quick.number3.value;
-    var currentAnswer4=document.Quick.number4.value;
-    var currenttAnswer5=document.Quick.number5.value;
-    var test=getElementById("test")
-    var result=document.getElementById("text")
-    if (currenttAnswer1=="Array") {score++};
+    var score=0;
+    var currentAnswer1 = document.Quick.number1.value;
+    var currentAnswer2 = document.Quick.number2.value;
+    var currentAnswer3 = document.Quick.number3.value;
+    var currentAnswer4 = document.Quick.number4.value;
+    var currenttAnswer5 = document.Quick.number5.value;
+
+    // var test=getElementById("test")
+    // var result=document.getElementById("text")
+    if (currentAnswer1=="Array") {score+=20};
     if (currentAnswer2=="Script") {score+=20};
     if (currentAnswer3=="const") {score+=20};
     if (currentAnswer4=="Correct") {score+=20};
-    
+    if (currenttAnswer5=="//" ){score+=20};
+    document.write(score);
+    // alert(score)
     
     if (score>=4) {
-    result.textContent= "Your score is 80%. Congratulations";
+   result.textContent= "Your score is 80%. Congratulations";
     } else{
 
     }
+
+
+
 }
-
-
